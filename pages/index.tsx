eb12ad48e230e0d51c928ea85399e4baa7242f7c
Samuel Wonjae Lee <samuellee0220@gmail.com>
2024-08-03 18:00:59 -0400

    Updated login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,17 +16,27 @@ const Entry: NextPage = () => {
       <Head>
         <title>Welcome to Matchify</title>
         <meta name="description" content="Entry point"/>
+        <link rel="icon" href="matchify_logo.png" type="image/gif" sizes="16x16"></link>
       </Head>
       <Background image="/bg1.jpg" alt="Concert Background"></Background>
+      {/* Make background image darker */}
+      <div className="dark-overlay"></div>
       <div className="flex flex-col h-full w-full justify-center items-center z-10">
-        <div className="z-10">OUR LOGO HERE</div>
-        <div className="text-center w-2/3 z-10 mt-10">
-          <p>Meet new people and attend events based on your music tastes using your Spotify profile!</p>
+        <div className="z-10">
+          <img src="matchify_logo.png"></img>
         </div>
-        <button className="w-2/3 bg-spotify-green h-10 z-10 rounded text-center z-10 mt-10" onClick={handleSpotifyAuth}>
+        <div className="text-center w-2/3 z-10 mt-10">
+          <p className="text-white text-3xl font-bold">Matchify</p>
+        </div>
+        <div className="text-center w-2/3 z-10 mt-10">
+          <p className="text-white">Meet new people and attend events based on your music tastes using your Spotify profile!</p>
+        </div>
+
+        {/* Continue With Spotify Button */}
+        <button className="w-1/3 bg-spotify-green h-10 z-10 rounded text-center z-10 mt-10" onClick={handleSpotifyAuth}>
           <div className="flex w-full h-full z-10">
             <div className="flex w-full items-center justify-center">
-              <p>Continue With Spotify</p>
+              <p className="text-white">Continue With Spotify</p>
               <img src="/spotify_logo_white.png" className="w-8 h-8 z-10 ml-2"></img>
             </div>
           </div>
