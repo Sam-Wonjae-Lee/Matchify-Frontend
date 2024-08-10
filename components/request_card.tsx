40a969df16cd9683da7376fffe17e93f0f768166
Riixardo <RicardoMonriguez2@gmail.com>
2024-08-10 00:32:46 -0700

    finished friend request page

--- components/request_card.tsx
+++ components/request_card.tsx
@@ -3,16 +3,20 @@ import React from "react";
 // Component for friend requests
 interface RequestCardProps {
     // TODO: Implement profile picture
+    image: string
     username: string;
     description: string;
     onAccept: () => void;
     onDecline: () => void;
 }
 
-const RequestCard: React.FC<RequestCardProps> = ({ username, description, onAccept, onDecline }) => {
+const RequestCard: React.FC<RequestCardProps> = ({ username, description, image, onAccept, onDecline }) => {
     return (
         <div className="p-4 rounded-lg shadow-md mb-4" style={{ backgroundColor: '#535353' }}>
-            <h2 className="text-xl font-bold text-white">{username}</h2>
+            <div className="flex items-center space-x-2">
+                <img src={image} className="h-12 w-12"></img>
+                <h2 className="text-xl font-bold text-white">{username}</h2>
+            </div>
             <p style={{ color: '#DADEDB' }}>{description}</p>
             <div className="mt-4 flex space-x-4">
                 <button 
