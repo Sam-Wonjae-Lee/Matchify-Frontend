3856828fb8c8ccd1394d5003a8cc1e697ad6875a
Samuel Wonjae Lee <samuellee0220@gmail.com>
2024-11-09 17:57:05 -0500

    Updated notification view

--- components/notification_card.tsx
+++ components/notification_card.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useState } from "react";
 
-// Component for notifications
 interface NotificationCardProps {
-    // TODO: May need to add date to when the notification was sent
-    iconPath: string;   // Path to icon image
-    message: string;    // Notification message
+    iconPath: string;
+    message: string;
     onDelete: () => void;
 }
 
@@ -13,14 +11,13 @@ const NotificationCard: React.FC<NotificationCardProps> = ({ iconPath, message, 
     const [position, setPosition] = useState({ x: 0, y: 0 });
     const [dragging, setDragging] = useState(false);
     const [startX, setStartX] = useState(0);
-    const maxDragDistance = -50; // Maximum distance to drag to the left
+    const maxDragDistance = -50;
 
     const handleMouseDown = (e: React.MouseEvent) => {
         setDragging(true);
         setStartX(e.clientX - position.x);
     };
 
-    // Needed for smaller dimensions when dragging
     const handleTouchStart = (e: React.TouchEvent) => {
         setDragging(true);
         setStartX(e.touches[0].clientX - position.x);
@@ -76,7 +73,7 @@ const NotificationCard: React.FC<NotificationCardProps> = ({ iconPath, message, 
     return (
         <div className="rounded-lg relative" style={{ backgroundColor: 'red' }}>
             <div
-                className="p-4 rounded-lg shadow-md mb-4 flex items-center justify-between relative z-10"
+                className="py-2 px-3 rounded-lg shadow-md flex items-center justify-between relative z-10"
                 style={{ backgroundColor: '#535353', transform: `translateX(${position.x}px)` }}
                 onMouseDown={handleMouseDown}
                 onTouchStart={handleTouchStart}
@@ -85,18 +82,17 @@ const NotificationCard: React.FC<NotificationCardProps> = ({ iconPath, message, 
                     <img 
                         src={iconPath} 
                         alt="Notification Icon" 
-                        className="w-8 h-8 mr-4"
-                        // Makes icon green
+                        className="w-4 h-4 mr-2"
                         style={{ filter: 'invert(48%) sepia(92%) saturate(749%) hue-rotate(85deg) brightness(92%) contrast(92%)' }} 
                     />
-                    <p className="text-white">{message}</p>
+                    <p className="text-white text-sm">{message}</p>
                 </div>
             </div>
-            <div className="absolute right-0 top-0 bottom-0 flex items-center pr-4 z-0">
+            <div className="absolute right-0 top-0 bottom-0 flex items-center pr-3 z-0">
                 <img
                     src="/delete_icon.svg"
                     alt="Delete Icon"
-                    className="w-6 h-6 cursor-pointer"
+                    className="w-4 h-4 cursor-pointer"
                     onClick={onDelete}
                 />
             </div>
