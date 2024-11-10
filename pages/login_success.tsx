de0a6be7abdceb6a643e351ec1b8b4859c3f7645
Samuel Wonjae Lee <samuellee0220@gmail.com>
2024-11-10 00:05:40 -0500

    Updated login success page view

--- pages/login_success.tsx
+++ pages/login_success.tsx
@@ -25,26 +25,28 @@ const LoginSuccess = () => {
     })
 
     return (
-        <div className="h-screen w-screen">
-            <Head>
-                <title>Login Success</title>
-                <meta name="description" content="Profile Creation Screen"/>
-                <link rel="icon" href="matchify_logo.svg" type="image/gif" sizes="16x16"></link>
-            </Head>
-            <Background image="/bg1.jpg" alt="Concert Background"></Background>
-            <div className="dark-overlay"></div>
-            {isCreation && (<div className="flex flex-col items-center justify-center h-full w-full">
-                <img src="/green_checkmark.svg" alt="green check mark" className="z-10 w-26 h-26"></img>
-                <h2 className="z-10 font-bold text-3xl text-white">Well Done!</h2>
-                <p className="w-2/3 text-center mt-5 z-10 text-white">You have successfully created your Matchify account!</p>
-                <button className="w-2/3 bg-spotify-green h-14 z-10 rounded text-center mt-20 text-white" onClick={handleHomeRedirect}>Home</button>
-            </div>)}
-            {!isCreation && (<div className="flex flex-col items-center justify-center h-full w-full">
-                <img src="/green_checkmark.png" alt="green check mark" className="z-10 w-26 h-26"></img>
-                <h2 className="z-10 font-bold text-3xl text-white">Logged In!</h2>
-                <p className="w-2/3 text-center mt-5 z-10 text-white">You have successfully logged in to your Matchify account!</p>
-                <button className="w-2/3 bg-spotify-green h-14 z-10 rounded text-center mt-20 text-white" onClick={handleHomeRedirect}>Home</button>
-            </div>)}
+        <div className="min-h-screen w-full flex items-center justify-center" style={{ backgroundColor: '#1C1C1C' }}>
+            <div className="w-[261px] h-[565px] bg-[#282828] relative">
+                <Head>
+                    <title>Login Success</title>
+                    <meta name="description" content="Profile Creation Screen"/>
+                    <link rel="icon" href="matchify_logo.svg" type="image/gif" sizes="16x16"></link>
+                </Head>
+                <img src="/bg1.jpg" alt="Concert Background" className="absolute w-full h-full object-cover object-center z-0"></img>
+                <div className="dark-overlay"></div>
+                {isCreation && (<div className="flex flex-col items-center justify-center h-full w-full">
+                    <img src="/green_checkmark.svg" alt="green check mark" className="z-10 w-20 h-20"></img>
+                    <h2 className="z-10 font-bold text-2xl text-white">Well Done!</h2>
+                    <p className="w-3/4 text-center mt-5 z-10 text-white text-sm">You have successfully created your Matchify account!</p>
+                    <button className="w-2/3 bg-spotify-green h-10 z-10 rounded text-center mt-20 text-white" onClick={handleHomeRedirect}>Home</button>
+                </div>)}
+                {!isCreation && (<div className="flex flex-col items-center justify-center h-full w-full">
+                    <img src="/green_checkmark.png" alt="green check mark" className="z-10 w-20 h-20"></img>
+                    <h2 className="z-10 font-bold text-3xl text-white">Logged In!</h2>
+                    <p className="w-3/4 text-center mt-5 z-10 text-white">You have successfully logged in to your Matchify account!</p>
+                    <button className="w-2/3 bg-spotify-green h-10 z-10 rounded text-center mt-20 text-white" onClick={handleHomeRedirect}>Home</button>
+                </div>)}
+            </div>
         </div>
     );
 
